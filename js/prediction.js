83c151fe1880c3bc7a931a480ef7846457698989
Mengyu Ji <mj2857@columbia.edu>
2019-04-28 23:01:46 -0400

    build web for data science

---
+++ js/prediction.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.dropdown').on('show.bs.dropdown', function(){
+    alert('The dropdown is about to be shown.');
+  });
+});
---
+++ js/search.js
@@ -0,0 +1,3 @@
+d3.csv("/data/2018.csv", function(data){
+    console.log(data);
+});
